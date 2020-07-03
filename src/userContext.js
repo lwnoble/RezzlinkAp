e3cfdeb601d592f603fd2299b9256f7d1eabd28c
lwnoble <lise.w.noble@gmail.com>
2020-07-03 08:32:19 -0400

    Update userContext.js

--- src/userContext.js
+++ src/userContext.js
@@ -10,46 +10,46 @@ function AuthProvider (props) {
   const [authDetermined, setAuthDetermined] = useState(false)
   const [fetching, setFetching] = useState(false)
   const [error, setError] = useState(false)
-
+  
   useEffect(() => {
-
-    const validJWT = getCookie('csrfToken')
+  
+  const validJWT = getCookie('csrfToken')
     if (validJWT) {
-        then(() => { setAuthDetermined(true) })
-        .catch(error => { console.log(error) })
+    then(() => { setAuthDetermined(true) })
+    .catch(error => { console.log(error) })
     } else {
-      setAuthDetermined(true)
+    setAuthDetermined(true)
     }
   }, [])
-
+  
   if (!authDetermined) return <Loader size={60}/>
-
+  
   const logIn = form => {
     setFetching(true)
     setError(false)
-
+    
     try{
-        const {user} = auth.signInWithEmailAndPasswordHandler ({email, password});
-        ;
+      const {user} = auth.signInWithEmailAndPasswordHandler ({email, password});
+      ;
       }
       catch(error)
-        { setError(error.response.data.reason) }
-      finally{() => { setFetching(false) 
-      }
+        { setError('Error Signing in with email and password'); }
+      finally{() => { setFetching(false)
+    }
   }
-
+  
   const signUp = form => {
     setFetching(true)
     setError(false)
     try{
-        const {user} = auth.createUserWithEmailAndPasswordHandler  ({email, password});
+      const {user} = auth.createUserWithEmailAndPasswordHandler ({email, password});
+      }
+      catch(error)
+        { setError('Error Signing up with email and password');}
+      finally{() => { setFetching(false)
     }
-    catch(error)
-      { setError(error.response.data.reason) }
-    finally{() => { setFetching(false) 
-    }
-}
-
+  }
+  
   const logOut = () => {
     firebase.auth().signOut().then(function() {
       // Sign-out successful.
@@ -57,11 +57,12 @@ function AuthProvider (props) {
       // An error happened.
     });
   }
-
+  
   return (
-    <UserContext.Provider
-      value={{ user, fetching, error, logIn, signUp, logOut }}
-    />
+  <UserContext.Provider
+    value={{ user, fetching, error, logIn, signUp, logOut }}
+  />
   )
-}}
+}
+  
 const useUser = () => useContext(UserContext)
